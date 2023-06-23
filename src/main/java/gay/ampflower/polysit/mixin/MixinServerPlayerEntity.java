8151747dcb5859910f9a284bfbfde134c3ecd601
Ampflower <ampflower@outlook.com>
2023-06-23 14:04:52 -0500

    Replace `teleport` with `requestTeleport`

--- src/main/java/gay/ampflower/polysit/mixin/MixinServerPlayerEntity.java
+++ src/main/java/gay/ampflower/polysit/mixin/MixinServerPlayerEntity.java
@@ -12,6 +12,7 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -26,6 +27,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
  **/
 @Mixin(ServerPlayerEntity.class)
 public abstract class MixinServerPlayerEntity extends PlayerEntity {
+	@Shadow
+	public abstract void requestTeleport(final double destX, final double destY, final double destZ);
+
 	public MixinServerPlayerEntity(final World world, final BlockPos pos, final float yaw,
 			final GameProfile gameProfile) {
 		super(world, pos, yaw, gameProfile);
@@ -38,6 +42,6 @@ public abstract class MixinServerPlayerEntity extends PlayerEntity {
 	 */
 	@Inject(method = "requestTeleportAndDismount", at = @At("RETURN"))
 	private void onDismount(double x, double y, double z, CallbackInfo ci) {
-		this.teleport(x, y, z);
+		this.requestTeleport(x, y, z);
 	}
 }
