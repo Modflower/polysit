65f00cac58c43084b3d5e5f030cf00126baef45c
Ampflower 🌺 <git@ampflower.gay>
2025-02-17 11:37:03 -0800

    Update to 1.21.2-1.21.4

--- src/main/java/gay/ampflower/polysit/mixin/MixinServerPlayerEntity.java
+++ src/main/java/gay/ampflower/polysit/mixin/MixinServerPlayerEntity.java
@@ -7,15 +7,22 @@
 package gay.ampflower.polysit.mixin;
 
 import com.mojang.authlib.GameProfile;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerPosition;
+import net.minecraft.network.packet.s2c.play.EntityPassengersSetS2CPacket;
+import net.minecraft.network.packet.s2c.play.PositionFlag;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * Forcefully teleports the player on dismount.
@@ -27,6 +34,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
  **/
 @Mixin(ServerPlayerEntity.class)
 public abstract class MixinServerPlayerEntity extends PlayerEntity {
+	@Shadow
+	public ServerPlayNetworkHandler networkHandler;
+
 	@Shadow
 	public abstract void requestTeleport(final double destX, final double destY, final double destZ);
 
@@ -42,6 +52,19 @@ public abstract class MixinServerPlayerEntity extends PlayerEntity {
 	 */
 	@Inject(method = "requestTeleportAndDismount", at = @At("RETURN"))
 	private void onDismount(double x, double y, double z, CallbackInfo ci) {
-		this.requestTeleport(x, y, z);
+		// Modified requestTeleport for also sending the current velocity.
+		// Enforces that the client can't just shove the player because there's a block
+		// edge to go to.
+		this.networkHandler.requestTeleport(new PlayerPosition(new Vec3d(x, y, z), this.getVelocity(), 0.F, 0.F),
+				PositionFlag.ROT);
+	}
+
+	/**
+	 * Forces a passenger update packet to the player, removing the chance for the
+	 * client to assert movement.
+	 */
+	@Inject(method = "startRiding", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;requestTeleport(Lnet/minecraft/entity/player/PlayerPosition;Ljava/util/Set;)V"))
+	private void onMount(Entity vehicle, boolean force, CallbackInfoReturnable<Boolean> ci) {
+		this.networkHandler.sendPacket(new EntityPassengersSetS2CPacket(vehicle));
 	}
 }
